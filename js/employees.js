52b1fa5fd65c9d19e9b00266bfbff2a927356b0a
harikrishnanakka <krishnaharin890@gmail.com>
2026-02-23 10:52:33 +0530

    'modifiedSomeJavascriptProperties'

--- js/employees.js
+++ js/employees.js
@@ -2,55 +2,35 @@
 function hidehandle() {
     const sidebar = document.querySelector(".sidebar");
     const main = document.querySelector(".main");
-    const handle = document.querySelector(".main img");
+    const handle = document.querySelector(".handle-pic");
+
     const spans = sidebar.querySelectorAll(".menu span");
     const titles = sidebar.querySelectorAll(".sidebar-title");
     const updateBox = sidebar.querySelector(".update-box");
-    const menuItems = sidebar.querySelectorAll(".menu li");
     const chevrons = sidebar.querySelectorAll(".menu i");
-    const logo = sidebar.querySelector(".logo");
+
     sidebar.classList.toggle("collapsed");
+    handle.classList.toggle("rotate");
 
     if (sidebar.classList.contains("collapsed")) {
-
-        // Collapse
         sidebar.style.width = "70px";
+        // sidebar.style.paddingTop = "70px"; 
         main.style.marginLeft = "85px";
-        handle.style.transform = "rotate(180deg)";
 
         spans.forEach(el => el.style.display = "none");
         titles.forEach(el => el.style.display = "none");
         chevrons.forEach(el => el.style.display = "none");
-
         if (updateBox) updateBox.style.display = "none";
 
-        menuItems.forEach(el => el.style.justifyContent = "center");
-
-        if (logo) {
-            logo.style.width = "40px";
-            logo.style.margin = "15px auto";
-        }
-
     } else {
-
-        // Expand
         sidebar.style.width = "240px";
+        // sidebar.style.paddingTop = "170px";  
         main.style.marginLeft = "250px";
-
-        handle.style.transform = "rotate(0deg)";
 
         spans.forEach(el => el.style.display = "inline");
         titles.forEach(el => el.style.display = "block");
         chevrons.forEach(el => el.style.display = "inline");
-
         if (updateBox) updateBox.style.display = "block";
-
-        menuItems.forEach(el => el.style.justifyContent = "flex-start");
-
-        if (logo) {
-            logo.style.width = "120px";
-            logo.style.margin = "20px";
-        }
     }
 }
 
